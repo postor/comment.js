bbc1c60c29b1471817699b740ce6b4eb24df5e2b
josh <postor@gmail.com>
2018-07-20 20:23:06 +0800

    temp commit

--- examples/full/components/Add.js
+++ examples/full/components/Add.js
@@ -1,46 +1,74 @@
 import React, { Component } from 'react'
-import addComment from 'local-comment/lib/add'
+import { add as addComment, patch as patchComment } from 'local-comment/lib/request'
 
-const commentapi = '/commentapi'
-const topic = 'global'
 
 class Add extends Component {
   constructor(props) {
     super(props)
     this.state = {
       commentText: '',
-      user: 'josh',
     }
   }
 
   render() {
-    const { commentText, user } = this.state
+    const { user, toUser } = this.props
+    const { commentText, } = this.state
     return (<div style={{
       position: 'relative'
     }}>
-      <input style={{
+      <span style={{
         width: '20%'
-      }}
-        value={user}
-        onChange={(e) => this.setState({ user: e.target.value })}
-      />:
+      }}>{user}</span>:
       <input style={{
         width: '60%'
       }}
         value={commentText}
         onChange={(e) => this.setState({ commentText: e.target.value })}
       />
-      <button onClick={() => {
-        addComment(commentapi, topic, {
-          content: commentText,
-          user,
-        })
-          .then(insertedObj => console.log({ insertedObj }))
-          .then(() => this.setState({ commentText: '' }))
-          .catch(error => console.log({ error }))
-      }}>submit</button>
+      <button onClick={() => this.handleAddComment()}>submit</button>
     </div>)
   }
+
+  handleAddComment() {
+    const { toUser } = this.props
+    if (toUser) {
+      this.replyComment(toUser)
+    } else {
+      this.addComment()
+    }
+
+  }
+
+  addComment() {
+    const { commentapi, topic, user } = this.props
+    const { commentText } = this.state
+    const [, promise1] = addComment(commentapi, topic, {
+      content: commentText,
+      user,
+    })
+    promise1
+      .then(([comment, feed]) => { })
+      .then(insertedObj => console.log({ insertedObj }))
+      .then(() => this.setState({ commentText: '' }))
+      .catch(error => console.log({ error }))
+  }
+
+
+  replyComment(toUser) {
+    const { commentapi, topic, user } = this.props
+    const { commentText } = this.state
+    const [, promise1] = addComment(commentapi, topic, {
+      content: commentText,
+      user,
+    })
+    
+    promise1
+      .then(([comment, feed]) => { })
+      .then(insertedObj => console.log({ insertedObj }))
+      .then(() => this.setState({ commentText: '' }))
+      .catch(error => console.log({ error }))
+  }
+
 }
 
 export default Add
